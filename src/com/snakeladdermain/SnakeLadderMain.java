db09d7f7f0cb91e6d96113c8126e8ac7f9dd609f
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-14 23:26:09 +0530

    Added: welcome message

---
+++ src/com/snakeladdermain/SnakeLadderMain.java
@@ -0,0 +1,9 @@
+package com.snakeladdermain;
+
+public class SnakeLadderMain 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("* * * WELCOME TO SNAKE AND LADDER GAME * * *");
+    }
+}
